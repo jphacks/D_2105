6e3edddb69891e133ec39c1001ce91db39a33d2b
Yoshino-Yukitaro <sushiki.az@gmail.com>
2021-10-23 20:31:45 +0900

    桜の主旋律を追加 #4

--- docker/opt/composer/create_music.py
+++ docker/opt/composer/create_music.py
@@ -165,6 +165,27 @@ def create_moderate_beach_melody(instruments_list):
     instruments_list.append(vibraphone)
 
 
+def create_cherry_melody(instruments_list):
+    """
+    パラメータが桜に該当した場合の主旋律を作成する
+
+    Parameters
+    ----------
+    instruments_list : pretty_midi.Pretty_midi.instruments
+        pretty_midi.Instrumentインスタンスを格納するリスト
+    """
+    organ = pm.Instrument(Instruments.DRAWBER_ORGAN)
+    cherry_melody_notes_list = \
+    [
+        (90,'C4',2,2.75),(90,'C4',2.75,3),(100,'D4',3,4),(100,'C4',4,5),(100,'F4',5,6),(100,'E4',6,8), #Happy Birthday to you
+        (90,'C4',8,8.75),(90,'C4',8.75,9),(100,'D4',9,10),(100,'C4',10,11),(100,'G4',11,12),(100,'F4',12,14), #Happy Birthday to you
+        (90,'C4',14,14.75),(90,'C4',14.75,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,17.75),(100,'F4',17.75,18),(95,'E4',18,18.75),(95,'E4',18.75,19),(90,'D4',19,21), #Happy Birthday dear ??
+        (100,'A#4',27,27.75),(100,'A#4',27.75,28),(100,'A4',28,29),(100,'F4',29,30),(100,'G4',31,32),(100,'F4',33,39) #Happy Birthday to you
+    ]
+    append_notes(notes = organ.notes, input_notes_list = peach_melody_notes_list)
+    instruments_list.append(organ)
+
+
 def create_main_melody(instruments_list, prime_value, secondary_value):
     """
     prime_valueに対応した主旋律を作成する
@@ -182,7 +203,7 @@ def create_main_melody(instruments_list, prime_value, secondary_value):
         #でバックよう
         create_great_ocean_main_melody(instruments_list)
     elif prime_value == 'cherry':
-        hoge = 'a'
+        create_cherry_melody
     elif prime_value == 'cat':
         hoge = 'a'
     elif prime_value == 'dog':

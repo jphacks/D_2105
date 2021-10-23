0bf37da78daaad210530955988804fcea761a574
KateSawada <nuksawada@gmail.com>
2021-10-23 11:34:00 +0900

    flask基盤作った

--- docker/opt/app.py
+++ docker/opt/app.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+# ページ表示関係
 @app.route('/')
 def hello():
-    greeting = "Hello World"
-    return greeting
+    return render_template(
+        "index.html",
+    )
+
+@app.route('/<id>/accept')
+def accept():
+    return render_template(
+        "accept.html",
+    )
+
+@app.route('/<id>/preview')
+def preview():
+    return render_template(
+        "preview.html",
+    )
+
+@app.route('/error')
+def error():
+    return render_template(
+        "error.html",
+    )
+# ページ表示関係 ここまで

1a481032f0b96651527fb45e63bfd245e19a7d0c
s-2010-119 <s.2010.119@gmail.com>
2021-10-23 16:39:06 +0900

    エラー回避 #10

--- docker/opt/app.py
+++ docker/opt/app.py
@@ -1,7 +1,63 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, send_file
+import re, uuid, os
+
 app = Flask(__name__)
 
+# ページ表示関係
 @app.route('/')
-def hello():
-    greeting = "Hello World"
-    return greeting
+def index():
+    return render_template(
+        "index.html",
+    )
+
+@app.route('/req', methods=['POST'])
+def req():
+    email = request.form.get('email')
+    twitter_id = request.form.get('twitter_id')
+
+    # メールアドレスのバリデーションを設定
+    email_pattern = "^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$"
+
+    if re.fullmatch(email_pattern, email) == None:
+        return redirect(url_for('index'))
+    else:
+        id = uuid.uuid4().hex
+        return redirect(url_for('accept', id=id))
+
+@app.route('/<id>/accept')
+def accept(id):
+    if os.path.isdir('./movie/' + id) == False:
+        return redirect(url_for('index'))
+
+    return render_template(
+        "accept.html",
+    )
+
+@app.route('/<id>/preview')
+def preview(id):
+    if os.path.isdir('./movie/' + id) == False:
+        return redirect(url_for('index'))
+
+    return render_template(
+        "preview.html",
+        id=id
+    )
+
+@app.route('/<id>/download')
+def download(id):
+    if os.path.isdir('./movie/' + id) == False:
+        return redirect(url_for('index'))
+
+    return send_file('./movie/' + id + '/happy_birthday.mp4', as_attachment=True)
+
+@app.route('/error')
+def error():
+    return render_template(
+        "error.html",
+    )
+
+# ページ表示関係 ここまで
+
+if __name__=='__main__':
+    port = 5000
+    app.run(host="0.0.0.0", port=port, debug=True)

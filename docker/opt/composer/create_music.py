a51c8f521275ca4762dfdb8fc61d5799565d2b9e
Yoshino-Yukitaro <sushiki.az@gmail.com>
2021-10-24 00:30:53 +0900

    南国の主旋律を作成する #4

--- docker/opt/composer/create_music.py
+++ docker/opt/composer/create_music.py
@@ -98,7 +98,7 @@ def create_default_main_melody(instruments_list):
         (90,'C4',14,14.75),(90,'C4',14.75,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,17.75),(100,'F4',17.75,18),(95,'E4',18,18.75),(95,'E4',18.75,19),(90,'D4',19,21), #Happy Birthday dear ??
         (100,'A#4',21,21.75),(100,'A#4',21.75,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,26) #Happy Birthday to you
     ]
-    ppend_notes(notes = piano.notes, input_notes_list = default_melody_notes_list)
+    append_notes(notes = piano.notes, input_notes_list = default_melody_notes_list)
     instruments_list.append(piano)
 
 
@@ -205,6 +205,28 @@ def create_cat_melody(instruments_list):
     ]
     append_notes(notes = orchestra_hit.notes, input_notes_list = cat_melody_notes_list)
     instruments_list.append(orchestra_hit)
+
+
+def create_tropical_melody(instruments_list):
+    """
+    パラメータが南国に該当した場合の主旋律を作成する
+
+    Parameters
+    ----------
+    instruments_list : pretty_midi.Pretty_midi.instruments
+        pretty_midi.Instrumentインスタンスを格納するリスト
+    """
+    xylophone = pm.Instrument(Instruments.XYLOPHONE)
+    tropical_melody_notes_list = \
+    [
+        (90,'C4',2,2.75),(90,'C4',2.75,3),(100,'D4',3,4),(100,'C4',4,5),(100,'F4',5,6),(100,'E4',6,8), #Happy Birthday to you
+        (90,'C4',8,8.75),(90,'C4',8.75,9),(100,'D4',9,10),(100,'C4',10,11),(100,'G4',11,12),(100,'F4',12,14), #Happy Birthday to you
+        (90,'C4',14,14.75),(90,'C4',14.75,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,17.75),(100,'F4',17.75,18),(95,'E4',18,18.75),(95,'E4',18.75,19),(90,'D4',19,21), #Happy Birthday dear ??
+        (100,'A#4',21,21.75),(100,'A#4',21.75,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,26) #Happy Birthday to you
+    ]
+    append_notes(notes = xylophone.notes, input_notes_list = tropical_melody_notes_list)
+    instruments_list.append(xylophone)
+
 
 
 def create_main_melody(instruments_list, prime_value, secondary_value):

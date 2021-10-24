17580b09ef78eab03df9e20d3d13c292655c6b3b
Yoshino-Yukitaro <sushiki.az@gmail.com>
2021-10-24 17:59:19 +0900

    メルヘンの主旋律を作成する #4

--- docker/opt/composer/create_music.py
+++ docker/opt/composer/create_music.py
@@ -243,10 +243,32 @@ def create_chunibyo_melody(instruments_list):
         (90,'C4',2,2.75),(90,'C4',2.75,3),(100,'D4',3,4),(100,'C4',4,5),(100,'F4',5,6),(100,'E4',6,8), #Happy Birthday to you
         (90,'C4',8,8.75),(90,'C4',8.75,9),(100,'D4',9,10),(100,'C4',10,11),(100,'G4',11,12),(100,'F4',12,14), #Happy Birthday to you
         (90,'C4',14,14.75),(90,'C4',14.75,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,17.75),(100,'F4',17.75,18),(95,'E4',18,18.75),(95,'E4',18.75,19),(90,'D4',19,21), #Happy Birthday dear ??
-        (100,'A#4',21,21.75),(100,'A#4',21.75,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,26) #Happy Birthday to you
+        (100,'A#4',21,21.75),(100,'A#4',21.75,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,28) #Happy Birthday to you
     ]
     append_notes(notes = trumpet.notes, input_notes_list = chunibyo_melody_notes_list)
     instruments_list.append(trumpet)
+
+
+def create_fairy_melody(instruments_list):
+    """
+    パラメータがメルヘンに該当した場合の主旋律を作成する
+
+    Parameters
+    ----------
+    instruments_list : pretty_midi.Pretty_midi.instruments
+        pretty_midi.Instrumentインスタンスを格納するリスト
+    """
+    bowed = pm.Instrument(Instruments.PAD5_BOWED)
+    fairy_melody_notes_list = \
+    [
+        (90,'C4',2,2.75),(90,'C4',2.75,3),(100,'D4',3,4),(100,'C4',4,4.8),(100,'C4',4.8,5),(100,'E4',5,5.2),(100,'F4',5.2,6),(100,'E4',6,8), #Happy Birthday to you
+        (90,'C4',8,8.75),(90,'C4',8.75,9),(100,'D4',9,10),(100,'C4',10,10.8),(100,'C4',10.8,11),(100,'E4',11,11.2),(100,'G4',11.2,12),(100,'F4',12,14), #Happy Birthday to you
+        (90,'C4',14,14.75),(90,'C4',14.75,14.81),(90,'F4',14.81,14.87),(90,'A4',14.87,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,17.75),(100,'F4',17.75,18),(95,'E4',18,18.75),(95,'E4',18.75,19),(90,'D4',19,21), #Happy Birthday dear ??
+        (100,'A#4',21,21.75),(100,'A#4',21.75,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,28) #Happy Birthday to you
+    ]
+    append_notes(notes = bowed.notes, input_notes_list = fairy_melody_notes_list)
+    instruments_list.append(bowed)
+
 
 
 def create_main_melody(instruments_list, prime_value, secondary_value):

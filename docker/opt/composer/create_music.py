705d73bd58d9d57f25eba3a914ca45896decdde4
Yoshino-Yukitaro <sushiki.az@gmail.com>
2021-10-25 19:13:54 +0900

    刀の主旋律を作成&おおらかな海を修正 #4

--- docker/opt/composer/create_music.py
+++ docker/opt/composer/create_music.py
@@ -117,7 +117,7 @@ def create_great_ocean_main_melody(instruments_list):
         (90,'C4',2,3),(100,'D4',3,4),(100,'C4',4,5),(100,'F4',5,6),(100,'E4',6,8), #Happy Birthday to you
         (90,'C4',8,9),(100,'D4',9,10),(100,'C4',10,11),(100,'G4',11,12),(100,'F4',12,14), #Happy Birthday to you
         (90,'C4',14,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,18),(95,'E4',18,19),(90,'D4',19,21), #Happy Birthday dear ??
-        (100,'A#4',21,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,26) #Happy Birthday to you
+        (100,'A#4',21,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,27) #Happy Birthday to you
     ]
     append_notes(notes = violin.notes, input_notes_list = great_ocean_melody_notes_list)
     instruments_list.append(violin)
@@ -291,6 +291,27 @@ def create_ghost_melody(instruments_list):
     instruments_list.append(goblins)
 
 
+def create_sword_melody(instruments_list):
+    """
+    パラメータが刀に該当した場合の主旋律を作成する
+
+    Parameters
+    ----------
+    instruments_list : pretty_midi.Pretty_midi.instruments
+        pretty_midi.Instrumentインスタンスを格納するリスト
+    """
+    shakuhachi = pm.Instrument(Instruments.SHAKUHACHI)
+    sword_melody_notes_list = \
+    [
+        (90,'C4',2,3),(100,'D4',3,4),(100,'C4',4,5),(100,'F4',5,6),(100,'E4',6,8), #Happy Birthday to you
+        (90,'C4',8,9),(100,'D4',9,10),(100,'C4',10,11),(100,'G4',11,12),(100,'F4',12,14), #Happy Birthday to you
+        (90,'C4',14,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,18),(95,'E4',18,19),(90,'D4',19,21), #Happy Birthday dear ??
+        (100,'A#4',21,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,27) #Happy Birthday to you
+    ]
+    append_notes(notes = shakuhachi.notes, input_notes_list = sword_melody_notes_list)
+    instruments_list.append(shakuhachi)
+
+
 def create_gun_melody(instruments_list):
     """
     パラメータが銃に該当した場合の主旋律を作成する
@@ -419,7 +440,7 @@ def create_main_melody(instruments_list, prime_value, secondary_value):
     elif prime_value == 'ghost':
         create_ghost_melody(instruments_list)
     elif prime_value == 'sword':
-        hoge = 'a'
+        create_sword_melody(instruments_list)
     elif prime_value == 'gun':
         create_gun_melody(instruments_list)
     elif prime_value == 'history':

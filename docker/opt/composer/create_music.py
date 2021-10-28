a5a89c7cadb00baf2eaba2710f3d0b1df9a94acc
Yoshino-Yukitaro <sushiki.az@gmail.com>
2021-10-28 19:15:23 +0900

    卓球の主旋律を作成 #4

--- docker/opt/composer/create_music.py
+++ docker/opt/composer/create_music.py
@@ -361,25 +361,35 @@ def create_baseball_melody(instruments_list):
     instruments_list.append(trumpet)
 
 
-def create_baseball_melody(instruments_list):
+def create_table_tennis_melody(instruments_list):
     """
-    パラメータがアイドルで、かつ、真夏のビーチに該当した場合の主旋律を作成する
+    パラメータが卓球に該当した場合の主旋律を作成する
 
     Parameters
     ----------
     instruments_list : pretty_midi.Pretty_midi.instruments
         pretty_midi.Instrumentインスタンスを格納するリスト
     """
-    brass_ensemble = pm.Instrument(Instruments.BRASS_SECTION)
-    baseball_melody_notes_list = \
+    piano = pm.Instrument(Instruments.ACOSTIC_GRAND_PIANO)
+    woodblock = pm.Instrument(Instruments.WOODBLOCK)
+    table_tennis_melody_notes_list = \
     [
         (90,'C4',2,2.75),(90,'C4',2.75,3),(100,'D4',3,4),(100,'C4',4,5),(100,'F4',5,6),(100,'E4',6,8), #Happy Birthday to you
         (90,'C4',8,8.75),(90,'C4',8.75,9),(100,'D4',9,10),(100,'C4',10,11),(100,'G4',11,12),(100,'F4',12,14), #Happy Birthday to you
-        (90,'C4',14,14.75),(90,'C4',14.75,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,17.75),(100,'F4',17.75,18),(95,'E4',18,18.75),(95,'E4',18.75,19),(90,'D4',19,21), #Happy Birthday dear ??
-        (100,'A#4',21,21.75),(100,'A#4',21.75,22),(100,'A4',22,23),(100,'F4',23,24),(100,'G4',24,25),(100,'F4',25,27) #Happy Birthday to you
+        (90,'C4',14,14.75),(90,'C4',14.75,15),(100,'C5',15,16),(100,'A4',16,17),(100,'F4',17,17.75),(100,'F4',17.75,18),(95,'E4',18,18.75),(95,'E4',18.75,19),(90,'D4',19,21), #Happy Birthday dear ?? 19->27までスマッシュ
+        (100,'A#4',27,27.75),(100,'A#4',27.75,28),(100,'A4',28,29),(100,'F4',29,30),(100,'G4',30,31),(100,'F4',31,33) #Happy Birthday to you
     ]
-    append_notes(notes = brass_ensemble.notes, input_notes_list = baseball_melody_notes_list)
-    instruments_list.append(brass_ensemble)
+    backing_param1 = [(80,'c#4',i*2,i*2+1) for i in range(1,9)]
+    backing_param2 = [(80,'C4',i*2-1,i*2) for i in range(1,10)]
+    backing_param3 = [(80,'C#4',j*2,j*2+1) for j in range(14,16)]
+    backing_param4 = [(80,'C4',j*2-1,j*2) for j in range(14,17)]
+    append_notes(notes = piano.notes, input_notes_list = table_tennis_melody_notes_list)
+    append_notes(notes = woodblock.notes, input_notes_list = backing_param1)
+    append_notes(notes = woodblock.notes, input_notes_list = backing_param2)
+    append_notes(notes = woodblock.notes, input_notes_list = backing_param3)
+    append_notes(notes = woodblock.notes, input_notes_list = backing_param4)
+    instruments_list.append(piano)
+    instruments_list.append(woodblock)
 
 
 def create_japanese_melody(instruments_list):
@@ -772,7 +782,7 @@ def create_main_melody(instruments_list, prime_value, secondary_value):
     elif prime_value == 'baseball':
         create_baseball_melody(instruments_list)
     elif prime_value == 'tabletennis':
-        hoge = 'a'
+        create_table_tennis_melody(instruments_list)
     elif prime_value == 'japanese':
         create_japanese_melody(instruments_list)
     elif prime_value == 'scandinavian':
